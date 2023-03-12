5961f4bd9354935ea9c40c6a1f156484432e9555
Matt Garry <mhgarry92@gmail.com>
2023-03-11 22:26:33 -0500

    added getCharacers function to isolate our variables and put them into later functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,29 @@
 // Assignment Code
+//added var password = document.querySelector("#password") to top for easier reference
 var generateBtn = document.querySelector("#generate");
-var password = document.querySelector("#password")
+var writePassword = document.querySelector("#password")
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function to put arguments in to generate our password
 
-  passwordText.value = password;
+
+
+// Deleted this function to start functions from scarth
+// Created function with options list to isolate variables we want in our password and create confirms and prompts to make user friendly
+function getCharacters() {
+    var addLowercase = confirm ("Press 'OK' to confirm you would like to include lowercase letters")
+    var addUppercase = confirm ("Press 'OK' to confirm you would like to inclue uppercase letters")
+    var addNumbers = confirm ("Press 'OK' to confirm you would like to include numbers")
+    var addSc = confirm ("press 'OK' to confirm you would like to include special characters")
+
+    var options = {
+      lower: addLowercase,
+      upper: addUppercase,
+      numbers: addNumbers,
+      sC: addSc
+
+    }
+console.log (options)
+    
 
 }
 
