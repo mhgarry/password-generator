f319fc8308abb1d85a7c522279607e804a5123ff
Matt Garry <mhgarry92@gmail.com>
2023-03-12 19:41:59 -0400

    wrote arrays differently and made password a var and changed the write password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,43 @@
- //2. validate input to ensure that the length is between 8-128 and ensure that at least
- //char type is being selected
- //3.display password on page
-var generateBtn = document.querySelector("#generate");
-//made function to generate the password
-function generatePassword (passOption){
-  return 'password text here'
-  var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+var generateBtn = document.querySelector("#generate")
+generateBtn.addEventListener("click", writePassword)
+
+console.log(generatePassword)
+
+var lower = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  var up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
   var special = ["'",' ','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','>','?','@','[',']','^','_','`','{','}','~']
-  var numeric = ['0','1','2','3','4','5','6','7','8','9']
+  var num = ['0','1','2','3','4','5','6','7','8','9']
   //creating blank array for character choices to later insert into for loop for selection of characters in password
   var characterChoices = []
-  //creating blank array of a list of all characters 
   var characterList = []
+//made function to generate the password
+function generatePassword (optionsObj){
+    //if statement saying if user picks certain prompts to user the arrays above to select characers genned
+    if (optionsObj.lower) {
+      characterChoices = characterChoices.concat(lower)
+    }
+
+    if (optionsObj.up) {
+      characterChoices = characterChoices.concat(up)
+    }
+    if (optionsObj.special) {
+      characterChoices = characterChoices.concat(special)
+    }
+    if (optionsObj.num) {
+      characterChoices = characterChoices.concat(num)
+    }
+    
+    for (var i = 8; i <= optionsObj.amount; i++) {
+      var randomSelection = characterChoices[Math.floor(Math.random() * 
+        characterChoices.length)]
+
+        characterList.push(randomSelection)
+    }
+    generatePassword(characterList)
 
 }
 
-console.log(generatePassword)
    //where i want to be and where i want the password to generate
    
   
@@ -26,26 +47,30 @@ console.log(generatePassword)
 //since the prompt is a string i have to convert the string into a interger which 
 //i did with the parseInit function
 function writePassword() {
-  var password = generatePassword()
-  var passwordText = document.querySelector("#password")
+ 
+  var password = document.querySelector("password")
+  
+  
 
-  passwordText.value = password
-
-  var addLowercase = confirm("Would you like to include lowercase letters in your password?")
-  var addUppercase = confirm("Would you like to inclue uppercase letters in your password?")
-  var addNumbers = confirm("Would you like to include numbers in your password?")
-  var addSpecialCharcters = confirm("Would you like to include special characters in your password?")
+  var lowercase = confirm("Would you like to include lowercase letters in your password?")
+  var uppercase = confirm("Would you like to inclue uppercase letters in your password?")
+  var numbers = confirm("Would you like to include numbers in your password?")
+  var specialChar = confirm("Would you like to include special characters in your password?")
   var passwordLength = prompt('Choose a password length: Must be at least 8 characters and no greater than 128 characters')
   var parsedAmount = parseInt(passwordLength)
 
-  var passwordText = {
-    lower: addLowercase,
-    upper: addUppercase,
-    numeric: addNumbers,
-    special: addSpecialCharcters,
+  var options = {
+    lower: lowercase,
+    up: uppercase,
+    num: numbers,
+    special: specialChar,
     amount: parsedAmount,
+  
   }
-  generatePassword(passwordText)
+  
+  
+  generatePassword(options)
+ 
 }
  //tested my passwordText options box and user prompts with console.log(passwordText) and 
  //my userprompts and functions worked as intended
@@ -53,4 +78,4 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+;
