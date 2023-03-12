fc9fc732fdf435a972ae7c9133b641d2a549ead8
Matt Garry <mhgarry92@gmail.com>
2023-03-12 01:51:18 -0500

    revising and trying to go back to beginning to solve problem

--- Develop/script.js
+++ Develop/script.js
@@ -7,9 +7,21 @@ var writePassword = document.querySelector('#passwords')
 
 console.log(writePassword)
 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
 //function to put arguments in to generate our password
 function writePassword(characterObj) {
   //created arrays for lowercase letter, uppercase letters, and special characters to be used in the generation of password
+  var
+  
+  
+  
   var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   var special = ["'",' ','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','>','?','@','[',']','^','_','`','{','}','~']
